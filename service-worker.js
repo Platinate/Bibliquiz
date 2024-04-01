1e7cdca770204e76b3a538a89499b0e01ee66b0d
Platinate <nathan@dado.be>
2024-04-01 15:25:29 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // Incrementing OFFLINE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 const OFFLINE_VERSION = 1;
-const CACHE_NAME = "offline";
+const CACHE_NAME = "bibliquiz";
 // Customize this with a different URL if needed.
 const OFFLINE_URL = "offline.html";
 
